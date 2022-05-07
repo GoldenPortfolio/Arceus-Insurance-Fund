ecd8fbafd4de9882837b0573e656ceabb037b88c
unknown <centuriitechnologies@gmail.com>
2022-05-07 19:08:51 -0400

    ADD: Header Front-end

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    domains: [
+      's2.coinmarketcap.com',
+      's3.coinmarketcap.com',
+      'encrypted-tbn0.gstatic.com',
+    ],
+  },
   reactStrictMode: true,
 }
